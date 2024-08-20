0d05cbba392e01f2e4723d4617d6beec4932fe9b
Bedanta D <bedanta@duck.com>
2024-08-20 21:14:13 +0530

    Add files via upload

--- config.h
+++ config.h
@@ -22,7 +22,7 @@ static char *colors[][3] = {
 };
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5" };
+static const char *tags[] = { "0", "1", "2", "3", "4", "5" };
 
 static const Rule rules[] = {
 	/* xprop(1):
